9a6c16c278edf12aa05124d8dee438e2a509f1fd
savkusamdekta <154704044+savkusamdekta@users.noreply.github.com>
2023-12-26 17:38:47 +0000

    Add terraform files

---
+++ modules/flux_bootstrap/output.tf
@@ -0,0 +1,13 @@
+#output "flux_namespace" {
+#  value = var.flux_namespace
+#}
+#variable "github_repository" {
+#  type        = string
+#  description = "GitHub repository to store Flux manifests"
+#}
+#
+#
+#variable "private_key" {
+#  type        = string
+#  description = "The private key used to authenticate with the Git repository"
+#}
---
+++ modules/tf-kind-cluster/output.tf
@@ -0,0 +1,15 @@
+output "client_key" {
+  value = kind_cluster.this.client_key
+}
+
+output "ca" {
+  value = kind_cluster.this.cluster_ca_certificate
+}
+
+output "crt" {
+  value = kind_cluster.this.client_certificate
+}
+
+output "endpoint" {
+  value = kind_cluster.this.endpoint
+}
