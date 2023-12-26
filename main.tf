0e28e4f4df7ce334e29b1dbf94134bee2ffbe906
savkusamdekta <154704044+savkusamdekta@users.noreply.github.com>
2023-12-26 18:02:37 +0000

    Make flux work

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ module "kind_cluster" {
 }
 
 module "flux_bootstrap" {
-  #source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
-  source            = "./modules/flux_bootstrap"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  #source            = "./modules/flux_bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
   config_host       = module.kind_cluster.endpoint
--- modules/tf-kind-cluster/terraform.tf
+++ modules/tf-kind-cluster/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     kind = {
-      source = "justenwalker/kind"
-      version = "0.17.0"
+      source  = "tehcyx/kind"
+      version = "0.1.1"
     }
   }
 }
